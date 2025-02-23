e0d6ccb58d5668d562c713219de230dba5db3471
Anthony Guerrero <anthonyguerrero62@outlook.com>
2025-02-22 22:24:55 -0800

    idk

--- main.lua
+++ main.lua
@@ -1,16 +1,28 @@
-function love.load()
-    target = {}
-    target.x = 300
-    target.y = 300
+_G.love = require("love")
 
-    print(target)
+function love.load()
+    _G.snake = {}
+    snake.x = 300
+    snake.y = 300
+    snake.speed = 3
 
 end
 
 function love.update(dt)
-
+    if love.keyboard.isDown("right") then
+        snake.x = snake.x + snake.speed
+    end
+    if love.keyboard.isDown("left") then
+        snake.x = snake.x - snake.speed
+    end
+    if love.keyboard.isDown("down") then
+        snake.y = snake.y + snake.speed
+    end
+    if love.keyboard.isDown("up") then
+        snake.y = snake.y - snake.speed
+    end
 end
 
 function love.draw()
-    love.graphics.rectangle("fill", target.x, target.y, 20, 20)
+    love.graphics.rectangle("fill", snake.x, snake.y, 20, 20)
 end
